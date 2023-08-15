d6f666bac4757fe93306bd10972055d95d2c92f2
Jeziel Jones <jeziel.chai@gmail.com>
2023-08-14 22:43:56 -0500

    test

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,13 +1,13 @@
-import NextAuth, { NextAuthOptions } from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
+// import NextAuth, { NextAuthOptions } from 'next-auth';
+// import GithubProvider from 'next-auth/providers/github';
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
-  ],
-};
+// export const authOptions: NextAuthOptions = {
+//   providers: [
+//     GithubProvider({
+//       clientId: process.env.GITHUB_ID as string,
+//       clientSecret: process.env.GITHUB_SECRET as string,
+//     }),
+//   ],
+// };
 
-export default NextAuth(authOptions);
+// export default NextAuth(authOptions);
